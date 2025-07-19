7ea81f409d206fa54231ed2c7c767aebf4aae8b9
MaksymLypai <maksym.lypai@students.plymouth.ac.uk>
2025-07-19 20:08:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+function sha512(message) {
+  return CryptoJS.SHA512(message).toString();
+}
+
+function encryptData(data, key) {
+  return CryptoJS.AES.encrypt(JSON.stringify(data), key).toString();
+}
+
+function downloadFile(content, filename) {
+  const blob = new Blob([content], { type: "application/json" });
+  const a = document.createElement("a");
+  a.href = URL.createObjectURL(blob);
+  a.download = filename;
+  a.click();
+}
 const themeToggle = document.getElementById("themeToggle");
 themeToggle.addEventListener("click", () => {
   document.body.classList.toggle("dark-theme");
